9d7c7893c8f34d04d99464f75ab0a56b6745850b
Gabrielgamas <gamas.enge@hotmail.com>
2025-08-05 15:34:29 -0300

    tratamento de exceções

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import cors from "cors";
+import { errorHandling } from "./middlewares/errorHandling";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => res.send("Hello, World"));
+app.use(errorHandling);
 
 export { app };
