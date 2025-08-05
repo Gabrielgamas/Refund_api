0cc0b0891bc434d51f744b74a1d6e4dcf54f13ab
Gabrielgamas <gamas.enge@hotmail.com>
2025-08-05 16:39:29 -0300

    CRIANDO ESTRUTURA PARA AUTHENTICAÇÃO

---
+++ src/controllers/sessionsController.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from "express";
+import { UserRole } from "@prisma/client";
+import { prisma } from "@/database/prisma";
+import { z } from "zod";
+import { AppError } from "@/utils/AppErrors";
+import { hash } from "bcrypt";
+
+class SessionsController {
+  async create(request: Request, response: Response) {
+    return response.json({ message: "ok" });
+  }
+}
+
+export { SessionsController };
